65075aeab0c3b15c97b5e047553e05be2a9f5ecc
Luigi Rocca <rocca@disi.unige.it>
2013-12-10 02:11:45 -0800

    smooth scripts 600px terrain

--- scripts/smooth-run.sh
+++ scripts/smooth-run.sh
@@ -8,17 +8,17 @@ if [ ! -d "$RES" ] ; then
 fi
 
 if [ $DO_RANGE_NORMAL = "yes" ]; then
-( set -x; $EXE $RANGE_OPTS $NORMAL_OPTS -i $SOURCE_DIR/$SOURCE_RANGE -o  "$RES"/"$RANGE"_"$NORMAL" -t "$RES"/"$RANGE"_"$NORMAL" > "$RES"/"$RANGE"_"$NORMAL".txt; )
+( set -x; $EXE $RANGE_OPTS $NORMAL_OPTS -i $SOURCE_DIR/$SOURCE_RANGE -o  "$RES"/"$RANGE"_"$NORMAL" -t "$RES"/"$RANGE"_"$NORMAL" > "$RES"/"$RANGE"_"$NORMAL".out.txt 2> "$RES"/"$RANGE"_"$NORMAL".err.txt; )
 fi
 
 if [ $DO_RANGE_SMOOTH = "yes" ]; then
-( set -x; $EXE $RANGE_OPTS $SMOOTH_OPTS -i $SOURCE_DIR/$SOURCE_RANGE -o  "$RES"/"$RANGE"_"$SMOOTH" -t "$RES"/"$RANGE"_"$SMOOTH" > "$RES"/"$RANGE"_"$SMOOTH".txt; )
+( set -x; $EXE $RANGE_OPTS $SMOOTH_OPTS -i $SOURCE_DIR/$SOURCE_RANGE -o  "$RES"/"$RANGE"_"$SMOOTH" -t "$RES"/"$RANGE"_"$SMOOTH" > "$RES"/"$RANGE"_"$SMOOTH".out.txt 2> "$RES"/"$RANGE"_"$SMOOTH".err.txt; )
 fi
 
 if [ $DO_DEM_NORMAL = "yes" ]; then
-( set -x; $EXE $DEM_OPTS $NORMAL_OPTS -i $SOURCE_DIR/$SOURCE_DEM -o  "$RES"/"$DEM"_"$NORMAL" -t "$RES"/"$DEM"_"$NORMAL" > "$RES"/"$DEM"_"$NORMAL".txt; )
+( set -x; $EXE $DEM_OPTS $NORMAL_OPTS -i $SOURCE_DIR/$SOURCE_DEM -o  "$RES"/"$DEM"_"$NORMAL" -t "$RES"/"$DEM"_"$NORMAL" > "$RES"/"$DEM"_"$NORMAL".out.txt 2> "$RES"/"$DEM"_"$NORMAL".err.txt; )
 fi
 
 if [ $DO_DEM_SMOOTH = "yes" ]; then
-( set -x; $EXE $DEM_OPTS $SMOOTH_OPTS -i $SOURCE_DIR/$SOURCE_DEM -o  "$RES"/"$DEM"_"$SMOOTH" -t "$RES"/"$DEM"_"$SMOOTH" > "$RES"/"$DEM"_"$SMOOTH".txt; )
+( set -x; $EXE $DEM_OPTS $SMOOTH_OPTS -i $SOURCE_DIR/$SOURCE_DEM -o  "$RES"/"$DEM"_"$SMOOTH" -t "$RES"/"$DEM"_"$SMOOTH" > "$RES"/"$DEM"_"$SMOOTH".out.txt 2> "$RES"/"$DEM"_"$SMOOTH".err.txt; )
 fi
