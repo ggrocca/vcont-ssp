82a61afdde77a9b89e8231861f8da9228922c703
Luigi Rocca <rocca@disi.unige.it>
2015-03-22 17:23:39 +0100

    perpoint checks number of fiducials

--- util/fiducialfacer/perpoint.sh
+++ util/fiducialfacer/perpoint.sh
@@ -3,7 +3,24 @@
 tab="distancetable2.dat"
 
 fiducials=`head $tab -n 13 | cut -f 3`
+meshnum=`tail -n 1 $tab | cut -f 2`
+#meshnum=$((meshnum+1))
+# 100 : mn = x : ln   x=(100/mn)ln    
+echo $meshnum
+#meshnum=108
 
+echo "check that all meshes have all fiducial points..."
+for (( m=1; m<=$meshnum; m++ )); do
+    lzm="$(printf "%03d" $m)"
+    #echo $lzm
+    fidnum=`grep Mesh_$lzm $tab | wc -l`
+    if [ $fidnum -ne "13" ]; then
+	echo "$lzm has $fidnum fiducial instead of 13"
+    fi
+done
+echo "done."
+echo ""
+echo "generate temporary tables and plots..."
 for fp in $fiducials; do
 
     echo $fp
@@ -13,7 +30,7 @@ for fp in $fiducials; do
 	grep $fp $tab | \
 	    cut -f "2,1,5" | \
 	    sort -g -k "3,3" | \
-	    awk 'BEGIN {printf("histvalue name number distance\n")}  {printf("%d %s\n", NR, $0)}'
+	    awk -v "m=$meshnum" 'BEGIN {printf("histvalue percent name number distance\n")}  {printf("%d %lf %s\n", NR, (100.0/m)*NR, $0)}'
     } > $fp.tmp.dat
 #    set +x
     
@@ -24,7 +41,7 @@ set title '$fp'
 #set xrange [0:40]
 #set yrange [0:120]
 plot '$fp.tmp.dat' \
-     using "distance":"histvalue" \
+     using "distance":"percent" \
      w lp
 _EOF_
 
@@ -33,11 +50,12 @@ set terminal pdfcairo size 20cm,20cm linewidth 4 rounded
 set output 'fix-$fp.pdf'
 set title '$fp'
 set xrange [0:40]
-set yrange [0:120]
+#set yrange [0:120]
 plot '$fp.tmp.dat' \
-     using "distance":"histvalue" \
+     using "distance":"percent" \
      w lp
 _EOF_
 
-    
 done
+echo "done."
+echo ""
